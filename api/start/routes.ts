9cb62fcf1791a0e0dfb1c9cade3f0f93354bedd9
Issac Cabral <issaccabrall@gmail.com>
2022-08-31 07:26:54 -0300

    init: add Docker on project and test db connection on api

---
+++ api/start/routes.ts
@@ -0,0 +1,15 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+// test health connection
+Route.get('/healthy', async ({ response }: HttpContextContract) => {
+  await Database.report().then(({ health }) => {
+    const { healthy, message } = health
+
+    if (healthy) return response.ok({ message })
+
+    return response.status(500).json({ message })
+  })
+})
+
