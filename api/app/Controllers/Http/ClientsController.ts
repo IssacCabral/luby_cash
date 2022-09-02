7ac35a895007a1a21e0e684666694eb4f15f8131
Issac Cabral <issaccabrall@gmail.com>
2022-09-02 13:59:44 -0300

    feat: add axios connection with evaluation microsservice

--- api/app/Controllers/Http/ClientsController.ts
+++ api/app/Controllers/Http/ClientsController.ts
@@ -2,11 +2,15 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import StoreValidator from 'App/Validators/Client/StoreValidator'
 
+import axios from 'axios'
+
 export default class ClientsController {
   public async store({request, response}: HttpContextContract) {
-    await request.validate(StoreValidator)
+    const result = await request.validate(StoreValidator)
 
-    return response.ok({message: 'tudo ok por enquanto'})
+    const {data} = await axios.get("http://evaluation:3334")
+
+    return response.ok({data})
   }
 
 
--- ms-evaluation/src/index.ts
+++ ms-evaluation/src/index.ts
@@ -4,12 +4,14 @@ import "./database/data-source"
 import express from 'express'
 import mainRouter from "./routes/mainRouter"
 import env from "./config/env"
+import corsConfig from "./config/corsConfig"
 
 const PORT = env.TYPEORM_SERVERPORT
 const app = express()
 
 app.use(express.json())
 app.use(mainRouter)
+app.use(corsConfig)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
