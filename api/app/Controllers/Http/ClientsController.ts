2cc79b5f9d80e36f76061e89d0a9a5893dc31dc4
Issac Cabral <issaccabrall@gmail.com>
2022-09-03 09:18:42 -0300

    refactor: remove returns and desnecessary try catchs

--- api/app/Controllers/Http/ClientsController.ts
+++ api/app/Controllers/Http/ClientsController.ts
@@ -10,24 +10,21 @@ import CreateApprovedClient from 'App/utils/CreateApprovedClient'
 export default class ClientsController {
   public async store({ request, response }: HttpContextContract) {
     const result = await request.validate(StoreValidator)
-    const {full_name, email, password, cpf} = result
+    const { full_name, email, password, cpf } = result
 
     try {
       const res = await axios.post('http://evaluation:3334/clients', result)
 
-      if (res.status == 201) {
-        if (res.data.status == 'desaproved') return response.status(400).json({ message: 'Insufficient average salary to be a client' })
+      if (res.data.status == 'desaproved') return response.status(400).json({ message: 'Insufficient average salary to be a client' })
 
-        try{
-          const client = await CreateApprovedClient({full_name, email, password, cpf})
-          
-          return response.created({clientCreated: await ReturnClientAfterCreate(client.cpf)})
-        } catch(error){
-          return response.badRequest({message: 'There was an error saving as a client', originalError: error.message})
-        }
+      try {
+        const client = await CreateApprovedClient({ full_name, email, password, cpf })
+
+        return response.created({ clientCreated: await ReturnClientAfterCreate(client.cpf) })
+      } catch (error) {
+        return response.badRequest({ message: 'There was an error saving as a client', originalError: error.message })
       }
 
-      return response.status(201).json(res.data)
     } catch (err) {
       return response.badRequest({ message: 'unable to become a client', originalMessage: err?.response?.data })
     }
