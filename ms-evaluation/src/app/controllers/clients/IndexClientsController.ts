85910581ace0f82d30f369bd32cf794b74b7ede1
Issac Cabral <issaccabrall@gmail.com>
2022-09-06 16:48:54 -0300

    feat: add clients pagination on index route in evaluation ms

--- ms-evaluation/src/app/controllers/clients/IndexClientsController.ts
+++ ms-evaluation/src/app/controllers/clients/IndexClientsController.ts
@@ -4,12 +4,39 @@ import { Client } from "../../models/Client";
 
 export class IndexClientsController{
     async index(request: Request, response: Response){
-        const {page, per_page, status} = request.query
+        const {page, per_page, status, noPaginate} = request.query
 
         const clientRepository = dataSource.getRepository(Client)       
 
-        const clients = await clientRepository.find({relations: {address: true}})
+        let clients: Client[]
 
-        return response.json({clients})
+        if(noPaginate){
+            clients = await clientRepository.find()
+            return response.json({allClients: clients})
+        }
+
+        if(!status){
+            clients = await clientRepository.find({
+                relations: {address: true},
+                take: Number(per_page) || 4,
+                skip: (Number(page) - 1) * Number(per_page) || 0,
+                cache: true
+            })
+            return response.json({clients})
+        }
+
+        if(status){
+            clients = await clientRepository.find({
+                where: {status: String(status)},
+                relations: {address: true},
+                take: Number(per_page) || 4,
+                skip: (Number(page) - 1) * Number(per_page) || 0,
+                cache: true
+            })
+            return response.json({clients})
+        }
+
+        
+
     }
 }
